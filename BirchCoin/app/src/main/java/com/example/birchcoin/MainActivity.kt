bb907e8e76eb4831622e780c8c1a2a5b9417c3f4
Przemysław Barłóg <przemek.barlog@interia.pl>
2019-05-14 20:09:03 +0200

    Added ads engine

--- BirchCoin/app/src/main/java/com/example/birchcoin/MainActivity.kt
+++ BirchCoin/app/src/main/java/com/example/birchcoin/MainActivity.kt
@@ -9,15 +9,23 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     var running = false
     var sensorManager:SensorManager? = null
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this, "ca-app-pub-4002528247773089~7509761805")
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 
